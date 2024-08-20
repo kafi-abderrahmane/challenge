324dccf320ba90effef061a5063cd8addc7e931c
kafi-abderrahmane <52238446+kafi-abderrahmane@users.noreply.github.com>
2024-08-20 18:01:43 +0100

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,13 @@
 export function formatDate(dateString: string | undefined): string {
   if (!dateString) return "";
-  // Convertit la chaîne en un objet Date
+  // Convert the string to a Date object
   const date = new Date(dateString);
 
-  // Extrait le jour, le mois et l'année
+  // Extract the day, month and year
   const day: string = String(date.getDate()).padStart(2, "0");
-  const month: string = String(date.getMonth() + 1).padStart(2, "0"); // Les mois sont indexés à partir de 0
+  const month: string = String(date.getMonth() + 1).padStart(2, "0"); 
   const year: number = date.getFullYear();
 
-  // Formate la date en "18.08.2024"
+  // Format the date
   return `${day}.${month}.${year}`;
 }
